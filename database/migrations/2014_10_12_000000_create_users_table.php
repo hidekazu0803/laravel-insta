797b8c1866c02ff4845ed0edb8e5c28ed77174bb
hidekazu0803 <hizekazu.nishi@gmail.com>
2022-06-22 20:19:05 +0900

    modify table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 191);
             $table->string('avatar', 50)->nullable();
             $table->string('password');
             $table->string('introduction',100)->nullable();
