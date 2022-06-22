b13594ff8859eaa6c6e3bb42e3076419c0aef2c5
hidekazu0803 <hizekazu.nishi@gmail.com>
2022-06-22 20:06:28 +0900

    modify table

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -35,5 +36,7 @@ class AppServiceProvider extends ServiceProvider
         });
         //
         paginator::useBootstrap();
+
+        Schema::defaultStringLength(191);
     }
 }
